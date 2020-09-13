245013b95382b3342cd2c613cdbb37a00d0990b0
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2020-09-13 19:32:29 +0800

    added zsh setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #/bin/bash
 
+
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+mv .zshrc ~/.zshrc
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 mv .vimrc ~/.vimrc
+
 echo open vim and use :PluginInstall command and python ~/.vim/bundle/Youcompleteme/install.py
